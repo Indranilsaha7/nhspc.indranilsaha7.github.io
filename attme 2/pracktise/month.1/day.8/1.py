65caaab1defc095bc291ae7a0b15361997e741e1
Indranil Saha <sahaindranil1984@gmail.com>
2026-07-04 14:37:30 +0600

    Upload files via Admin Portal (1.py)

---
+++ attme 2/pracktise/month.1/day.8/1.py
@@ -0,0 +1,16 @@
+word = "Python"
+# P = 0
+# y = 1
+# t = 2
+# h = 3
+# o = 4
+# n = 5
+
+# প্রথম অক্ষরটি প্রিন্ট করা হচ্ছে
+print(word[0])  # Output: P
+
+# চার নম্বর পজিশনের অক্ষর প্রিন্ট করা হচ্ছে
+print(word[3])  # Output: h
+
+# নেগেটিভ ইনডেক্সিং (Negative Indexing)
+print(word[-1]) # Output: n (সবচেয়ে শেষের অক্ষর)
