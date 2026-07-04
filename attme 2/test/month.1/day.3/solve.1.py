62a6551837b7199a63988e4b5499f0a1954b8860
Indranil Saha <sahaindranil1984@gmail.com>
2026-07-04 14:36:58 +0600

    Upload files via Admin Portal (solve.1.py)

---
+++ attme 2/test/month.1/day.3/solve.1.py
@@ -0,0 +1,7 @@
+# variables and the dataset
+points_per_win  = 10
+wins = 7
+penalty = 5
+# calculate the total score
+total_score = (points_per_win * wins) - penalty
+print(total_score) # print the output
