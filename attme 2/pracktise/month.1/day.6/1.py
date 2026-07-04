1cb100a3c1848f33658eb4c17fb69ee463c5d334
Indranil Saha <sahaindranil1984@gmail.com>
2026-07-04 14:37:37 +0600

    Upload files via Admin Portal (1.py)

---
+++ attme 2/pracktise/month.1/day.6/1.py
@@ -0,0 +1,8 @@
+my_score = 90
+target = 100
+
+# চেক করছি আমার স্কোর টার্গেটের সমান কি না
+print(my_score == target)  # Output: False
+
+# চেক করছি টার্গেট আমার স্কোরের চেয়ে বড় কি না
+print(target > my_score)   # Output: True
