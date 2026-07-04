bcb8d4babd1e4f591c9c32602239ff7cafd0a8aa
Indranil Saha <sahaindranil1984@gmail.com>
2026-07-04 14:37:03 +0600

    Upload files via Admin Portal (solve.1.py)

---
+++ attme 2/test/month.1/day.2/solve.1.py
@@ -0,0 +1,9 @@
+# variables: 
+mission = "NHSPC Champion" # string
+target_year = 2027 # integer
+win_rate = 100.0 # float
+mission_started = True # boolean
+print(mission)
+print(target_year)
+print(win_rate)
+print(mission_started)
