0a2634202d263af78b52eaa2e106e92d881adc74
Indranil Saha <sahaindranil1984@gmail.com>
2026-07-04 14:37:47 +0600

    Upload files via Admin Portal (1.py)

---
+++ attme 2/pracktise/month.1/day.3/1.py
@@ -0,0 +1,8 @@
+a = 10
+b = 3
+
+print(a + b)  # Output: 13
+print(a / b)  # Output: 3.3333333333333335 (Float Division)
+print(a // b) # Output: 3 (Floor Division)
+print(a % b)  # Output: 1 (১০ কে ৩ দিয়ে ভাগ করলে ভাগশেষ ১ থাকে)
+print(b ** 2) # Output: 9 (৩ এর স্কয়ার)
