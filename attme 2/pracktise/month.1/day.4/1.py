7748595d3b52eb4025c081ca96b972bcec98a012
Indranil Saha <sahaindranil1984@gmail.com>
2026-07-04 14:37:44 +0600

    Upload files via Admin Portal (1.py)

---
+++ attme 2/pracktise/month.1/day.4/1.py
@@ -0,0 +1,5 @@
+# ইউজারের কাছ থেকে নাম ইনপুট নিয়ে একটি variable-এ রাখা হচ্ছে
+player_name = input("Enter your name: ")
+
+# এরপর সেই নামটি প্রিন্ট করা হচ্ছে
+print("Welcome to NHSPC, " + player_name)
